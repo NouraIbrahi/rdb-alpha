d13d7cccad0789ee4b7b870b5fb8d0fc97e2bc85
Nora Al-Hzim <124936214+NouraIbrahi@users.noreply.github.com>
2026-05-05 12:42:39 -0400

    Initial setup of second_database: created tables and managed columns

---
+++ second_database.sql
@@ -0,0 +1,83 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.22 (Ubuntu 12.22-0ubuntu0.20.04.4)
+-- Dumped by pg_dump version 12.22 (Ubuntu 12.22-0ubuntu0.20.04.4)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE second_database;
+--
+-- Name: second_database; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE second_database WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE second_database OWNER TO freecodecamp;
+
+\connect second_database
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: first_table; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.first_table (
+);
+
+
+ALTER TABLE public.first_table OWNER TO freecodecamp;
+
+--
+-- Name: second_table; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.second_table (
+    first_column integer,
+    id integer
+);
+
+
+ALTER TABLE public.second_table OWNER TO freecodecamp;
+
+--
+-- Data for Name: first_table; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+
+
+--
+-- Data for Name: second_table; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+
+
+--
+-- PostgreSQL database dump complete
+--
+
